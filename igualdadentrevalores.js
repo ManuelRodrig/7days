a9a9ce8fc5f149a58022c93d0ac29c71c924e3d4
ManuelRodrig <72423035+ManuelRodrig@users.noreply.github.com>
2024-08-15 23:08:30 -0500

    7daysofcodes

---
+++ igualdadentrevalores.js
@@ -0,0 +1,94 @@
+/*console.log( false == '0' );
+
+console.log( null == undefined );
+
+console.log( " \t\r\n" == 0 );
+
+console.log( ' ' == 0 );
+*/
+
+let numeroUn = 1
+
+let stringUn = '1'
+
+let numeroTreinta = 30
+
+let stringTreinta = '30'
+
+let numeroDiez = 10
+
+let stringDiez = '10'
+
+
+console.log('Con dos signos de igualdad == igualdad')
+
+if (numeroUn == stringUn) {
+
+  console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero no son identicos')
+
+} else {
+
+  console.log('Las variables numeroUn y stringUn no son identicos, tienen diferentes formatos')
+
+}
+
+ 
+
+if (numeroTreinta == stringTreinta) {
+
+  console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor, pero no son identicos')
+
+} else {
+
+  console.log('Las variables numeroTreinta y stringTreinta no son identicos, tienen diferentes formatos')
+
+}
+
+ 
+
+if (numeroDiez == stringDiez) {
+
+  console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero no son identicos')
+
+} else {
+
+  console.log('Las variables numeroDiez y stringDiez no son identicos, tienen diferentes formatos')
+
+}
+
+console.log('Con tres signos de igualdad === identicos')
+
+if (numeroUn === stringUn) {
+
+    console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero no son identicos')
+  
+  } else {
+  
+    console.log('Las variables numeroUn y stringUn no son identicos, tienen diferentes formatos')
+  
+  }
+  
+   
+  
+  if (numeroTreinta === stringTreinta) {
+  
+    console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor, pero no son identicos')
+  
+  } else {
+  
+    console.log('Las variables numeroTreinta y stringTreinta no son identicos, tienen diferentes formatos')
+  
+  }
+  
+   
+  
+  if (numeroDiez === stringDiez) {
+  
+    console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero no son identicos')
+  
+  } else {
+  
+    console.log('Las variables numeroDiez y stringDiez no son identicos, tienen diferentes formatos')
+  
+  }
+  
